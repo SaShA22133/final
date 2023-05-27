c05e44c5c98001c406037f9869db6e9b71aae4a0
SaShA22133 <130833964+SaShA22133@users.noreply.github.com>
2023-05-27 17:40:25 +0700

    Update final.py

--- final.py
+++ final.py
@@ -1,8 +1,8 @@
-import random
+import pyautogui, random,  time, keyboard
 answer = random.randint(1,101)
 popit = 1
 
-num = int(input(" угадайте число "))
+num = int(input(" Привет! Если угадаешь случайное число то можешь посмотреть ютуб "))
 while num != answer:
     popit = popit + 1
     if num > answer:
@@ -11,4 +11,24 @@ while num != answer:
          print("загаданное число больше", num)
     print("попытка номер",popit)
     num = int(input(" попробуйте еща раз угадать число "))
+    
+   
 print("вы угадали число", answer, "за",popit, "попыток"  )
+num = input(" молодец! А теперь что бы ты хотел посмотреть в ютубе? ")
+pyautogui.moveTo(1250, 10)
+
+pyautogui.click()
+time.sleep(1)
+pyautogui.click()
+pyautogui.click()
+time.sleep(2)
+
+keyboard.write('https://www.youtube.com/')
+pyautogui.hotkey("enter")
+pyautogui.moveTo(850, 100)
+time.sleep(4)
+pyautogui.click()
+keyboard.write(num)
+pyautogui.hotkey("enter")
+
+
